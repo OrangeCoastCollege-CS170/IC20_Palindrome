d07ac6e0df02957e03488e1b13f42ecc02fec4f5
Badajoz <sbadajoz@student.cccd.edu>
2016-11-09 11:55:49 -0800

    Completed Palindrome class

--- src/edu/orangecoastcollege/sbadajoz/ic20/Palindrome.java
+++ src/edu/orangecoastcollege/sbadajoz/ic20/Palindrome.java
@@ -1,6 +1,34 @@
 package edu.orangecoastcollege.sbadajoz.ic20;
+/*
+Badajoz, Seve
+CS A170
+November 9, 2016
 
-public class Palindrome
-{
+IC20
+*/
 
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static boolean isPalindrome(String check){
+        for(int i = 0; i < check.length(); i++) {
+            if(check.charAt(i) !=check.charAt(check.length() - i - 1)) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String userInput;
+        boolean active = true;
+        
+        while(active) {
+            System.out.print("Please enter word or phrase to see if it's a palindrome (type quit to end the program): ");
+            userInput = input.nextLine();
+            if(isPalindrome(userInput)) System.out.println(userInput + " is a a palindrome!");
+            active = !userInput.toLowerCase().equals("quit");
+        }
+        System.out.println("Thank You, goodbye");
+    }
 }
